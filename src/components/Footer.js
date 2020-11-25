139e9d580183f85b17efd206b0f4c00b79b3b259
Karishma <karishmapatel93@hotmail.com>
2020-11-26 12:12:54 +1300

    add social media links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {
+  FaLinkedin,
+  FaFontAwesome,
+} from "react-icons/fa"
+import { GoMarkGithub } from 'react-icons/go'
 
 const Footer = () => {
   return (
     <footer>
+      <div className="social-media-links">
+        <a href="https://www.linkedin.com/in/karishma-patel27/"
+          className="social-link" rel="noreferrer" target="_blank">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/karishmapatel27"
+          className="social-link" rel="noreferrer" target="_blank">
+          <GoMarkGithub />
+        </a>
+      </div>
       <span>Created By <span className="footer-name">Karishma Patel</span> | <span className="far fa-copyright"></span> 2020 All rights reserved.</span>
     </footer>
   )
