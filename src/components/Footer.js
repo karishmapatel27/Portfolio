c8923a6598330e6f0c2bf6656600230c6a90ff20
Karishma <karishmapatel93@hotmail.com>
2020-11-28 17:22:07 +1300

    change colors and add logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,23 @@ import { GoMarkGithub } from 'react-icons/go'
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="social-media-links">
-        <a href="https://www.linkedin.com/in/karishma-patel27/"
-          className="social-link" rel="noreferrer" target="_blank">
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/karishmapatel27"
-          className="social-link" rel="noreferrer" target="_blank">
-          <GoMarkGithub />
-        </a>
-      </div>
-      <span>Created By <span className="footer-name">Karishma Patel</span> | <span className="far fa-copyright"></span> 2020 All rights reserved.</span>
-    </footer>
+    <>
+      <div className="line"></div>
+      <footer>
+
+        <div className="social-media-links">
+          <a href="https://www.linkedin.com/in/karishma-patel27/"
+            className="social-link" rel="noreferrer" target="_blank">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/karishmapatel27"
+            className="social-link" rel="noreferrer" target="_blank">
+            <GoMarkGithub />
+          </a>
+        </div>
+        <span>Created By <span className="footer-name">Karishma Patel</span> | <span className="far fa-copyright"></span> 2020 All rights reserved.</span>
+      </footer>
+    </>
   )
 }
 
