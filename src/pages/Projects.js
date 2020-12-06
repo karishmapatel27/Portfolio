55def3e3e669a9da81ed6c5c5968b4ed34efa281
Karishma <karishmapatel93@hotmail.com>
2020-12-06 15:38:10 +1300

    modify project and styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -18,11 +18,9 @@ const Projects = () => {
               <p className="project-desc">
                 GiveMe is the final project that I created with an exceptional team and presented at our Bootcamp's graduation. As a team, we created a platform that enables kiwis to donate unwanted goods. User can list unwanted items and person interested in that item can then get the contact details of the product owner.</p>
               <div className="project-stack">
-                <span>React</span>
                 <span>React hooks</span>
                 <span>React Context</span>
                 <span>JavaScript</span>
-                <span>HTML</span>
                 <span>CSS</span>
                 <span>Authenticare</span>
                 <span>SQLite3</span>
@@ -50,8 +48,8 @@ const Projects = () => {
               <span className="project-number">02</span>
               <h3>Tell Story</h3>
               <p className="project-desc">
-                TellStory is a simple and entertaining story generator game, based on an idea given by our team member. This game was our Friday project of week three in Bootcamp.<br /><br />
-                This game was our way to practise newly learnt skills such as RDBMS and Knex.js.</p>
+                TellStory is a simple and entertaining story generator game, based on an idea given by our team member. This game was our Friday project of week three in Bootcamp.<br />
+                This game was our way to practise newly learnt skills such as RDBMS, Knex.js and Handlebars.</p>
               <div className="project-stack">
                 <span>Express-Handlebars</span>
                 <span>Express.js</span>
@@ -82,13 +80,14 @@ const Projects = () => {
               <span className="project-number">03</span>
               <h3>Meme Generator</h3>
               <p className="project-desc">
-                This is a single-day project where our team decided to create a meme builder website. We built his project to test out knowledge on React and React router. To add some humour, we decided to use images of our teachers and students from EDA.</p>
+                This is a single-day project where our team decided to create a meme builder website. We built this project to test out knowledge on React and React router. To add some humour, we decided to use images of our teachers and students from EDA.</p>
               <div className="project-stack">
                 <span>React</span>
                 <span>React Router</span>
                 <span>Canvas</span>
                 <span>Javascript</span>
                 <span>CSS</span>
+                <spa>semantic-ui</spa>
                 <span>HTML</span>
               </div>
               <div className="project-links">
@@ -111,13 +110,14 @@ const Projects = () => {
           <div className="column right">
             <div className="project-info">
               <span className="project-number">04</span>
-              <h3>WeatherPerson</h3>
+              <h3>Weather App</h3>
               <p className="project-desc">
-                The simple weather app was created in week five of out Bootcamp to practice React and API.</p>
+                This project was created in week five of our bootcamp to practice our newly learnt skills such as React and API. As a team we decided to create this simple weather app which uses API from openweathermap.</p>
               <div className="project-stack">
-                <span>React</span>
+                <span>React.js</span>
                 <span>JavaScript</span>
                 <span>CSS</span>
+                <span>REST API</span>
               </div>
               <div className="project-links">
                 <a href="https://github.com/karishmapatel27/WhetherPerson" rel="noreferrer" target="_blank">
@@ -134,26 +134,23 @@ const Projects = () => {
         <br />
         <div className="project-content">
           <div className="column left">
-            <img src="netflix-clone.png" alt="profile" />
+            <img src="firstwebsite.png" alt="profile" />
           </div>
           <div className="column right">
             <div className="project-info">
               <span className="project-number">05</span>
-              <h3>Netflix Clone</h3>
+              <h3>My First Website</h3>
               <p className="project-desc">
-                This is a Netflix clone I created to practice React and API.</p>
+                My very first website! I built this site suring my bootcamp foundation using HTML and CSS. During development I realised it takes a lotmore then just writing code to create a functional and visualy aesthitic website. It was the beginin of a lifelong journey, and looking back I realise the progress I have made in a few short months. </p>
               <div className="project-stack">
-                <span>React</span>
-                <span>React hooks</span>
-                <span>JavaScript</span>
-                <span>Axios</span>
+                <span>HTML</span>
                 <span>CSS</span>
               </div>
               <div className="project-links">
-                <a href="https://github.com/karishmapatel27/netflix_clone" rel="noreferrer" target="_blank">
+                <a href="https://github.com/karishmapatel27/karishmapatel-myfirstwebsite" rel="noreferrer" target="_blank">
                   <FaGithubSquare className="project-icon" />
                 </a>
-                <a href="https://react-meme-builder.netlify.app/#/" rel="noreferrer" target="_blank" >
+                <a href="https://karishmapatel27.github.io/karishmapatel-myfirstwebsite/" rel="noreferrer" target="_blank" >
                   <FaShareSquare className="project-icon" />
                 </a>
               </div>
